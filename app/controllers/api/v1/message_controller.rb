1229277de90b3c544293580e42b6f30ee77c9302
wandergithub <wandergonzalez0@hotmail.com>
2023-01-03 15:33:56 -0400

    Create api/v1/message controller

---
+++ app/controllers/api/v1/message_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::MessageController < ApplicationController
+    def index
+      render json: rand_message
+    end
+  
+    
+    private
+  
+    def rand_message
+      messages = Message.all
+      messages.sample
+    end
+  end
